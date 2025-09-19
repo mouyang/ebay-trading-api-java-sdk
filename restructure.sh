e4f4faf72228d02fe10e24306edb217c1d05e64c
Matthew Ouyang <mouyang@users.noreply.github.com>
2025-09-19 13:03:57 -0400

    REFACTOR into functions
    in preparation for creating a trading-api-ui module because the file
    copying step is a common step between that and trading-api-sdkcore and
    any new module for that matter
    
    make all of the file copying methods run in parallel and do the final
    build at the end

--- restructure.sh
+++ restructure.sh
@@ -5,64 +5,94 @@ cleanup() {
     cd $pwd
 }
 
+# clear out everything and point to the desired branch
+reset_repo() {
+    git restore --staged .
+    git checkout .
+    git clean -fd
+    git checkout $1
+}
+
+copy_to_root() {
+    # new .gitignore entries
+    echo "" >> .gitignore
+    cat $pwd/gitignore >> .gitignore
+    # parent pon
+    copy_maven_pom
+}
+
+copy_to_eBLBaseComponents() {
+    module_dir="trading-api-eBLBaseComponents"
+    copy_maven_pom $module_dir
+
+    ## generate types from WSDL + remove related java source files generated from WSDL
+    mkdir -p $module_dir $module_dir/src/jaxws
+    cp $pwd/$module_dir/pom.xml $module_dir
+    cp $pwd/$module_dir/src/jaxws/custom-binding.xml \
+        $pwd/$module_dir/src/jaxws/jaxb-binding.xjb \
+        $module_dir/src/jaxws
+}
+
+copy_to_sdkcore() {
+    module_dir="trading-api-sdkcore"
+    copy_maven_pom $module_dir
+    copy_source_files $module_dir
+    ## Drop anything core or helper class that uses a *Call class or a *Type that does not have a source file in the 
+    ## source repo.  These removed classes may be reintroduced later but will need to be evaluated on a case-by-case basis.
+    ##
+    ## Files are removed individually because it is hard to join class references in files with Call/Type classes.
+    rm $module_dir/$mvn_src_dir/com/ebay/sdk/helper/GetCategoryFeaturesHelper.java \
+        $module_dir/$mvn_src_dir/com/ebay/sdk/helper/cache/CategoriesDownloader.java \
+        $module_dir/$mvn_src_dir/com/ebay/sdk/helper/cache/DetailsDownloader.java \
+        $module_dir/$mvn_src_dir/com/ebay/sdk/helper/cache/FeaturesDownloader.java \
+        $module_dir/$mvn_src_dir/com/ebay/sdk/helper/eBayDetailsHelper1.java \
+        $module_dir/$mvn_src_dir/com/ebay/sdk/helper/ui/DialogAccount.java
+
+    ## Temporarily remove anything related to Java AWT + Swing in order to focus first on the API.  API and UI should be 
+    ## compiled and packaged separately.  Catches import statements but any fully-qualified references will need to be 
+    ## addressed individually.
+    cp $pwd/$module_dir/$mvn_src_dir/com/ebay/sdk/ApiCall.java $pwd/$module_dir/$mvn_src_dir/com/ebay/sdk/ApiCredential.java \
+        $module_dir/$mvn_src_dir/com/ebay/sdk
+    find $module_dir -name *.java | xargs grep -l -e "^import java\.awt\..*;$" -e  "^import javax\.swing\..*;$" | xargs rm
+}
+
+copy_maven_pom() {
+    module_dir=${1:-.}
+    mkdir -p $module_dir
+    cp $pwd/$module_dir/pom.xml $module_dir
+}
+
+copy_source_files() {
+    mkdir -p $1/$mvn_src_dir/com/ebay/sdk
+    cp -r source/core/src/com/ebay/sdk $1/$mvn_src_dir/com/ebay/sdk
+    mkdir -p $1/$mvn_src_dir
+    cp -r source/helper/src/com $1/$mvn_src_dir
+
+    ## Fortunately this works because the imports follow a consistent spacing format.  OpenRewrite would have been 
+    ## preferred to refactor this but attempting use this was successful.
+    find $1/$mvn_src_dir -name *.java | xargs sed -i -e \
+        "s/^import javax\.xml\.ws/import jakarta.xml.ws/" \
+        -e "s/^import javax\.xml\.soap/import jakarta.xml.soap/" \
+        -e "s/^import javax\.xml\.bind/import jakarta.xml.bind/"
+}
+
 trap cleanup EXIT
 
 pwd=`pwd`
 target=$1
 branch=${2:-main}
 
+mvn_src_dir="src/main/java"
+
 # easier to move to the target repo and restore after instead of staying in this repo because specifying the target 
 # directory in for each app (git, mvn, etc if it can be specified at all)
-cd $target
-# start from a clean target
-git restore --staged .
-git checkout .
-git clean -fd
-git checkout $branch
+cd $target && reset_repo $branch
 
-# new .gitignore entries
-echo "" >> .gitignore
-cat $pwd/gitignore >> .gitignore
-# remove all class and jar files that can be generated from source
-find . -name *.class -o -name *.jar | xargs rm
-# eBLBaseComponents
-## generate types from WSDL + remove related java source files generated from WSDL
-mkdir -p trading-api-eBLBaseComponents trading-api-eBLBaseComponents/src/jaxws
-cp $pwd/pom.xml .
-cp $pwd/trading-api-eBLBaseComponents/pom.xml trading-api-eBLBaseComponents
-cp $pwd/trading-api-eBLBaseComponents/src/jaxws/custom-binding.xml \
-	$pwd/trading-api-eBLBaseComponents/src/jaxws/jaxb-binding.xjb \
-	trading-api-eBLBaseComponents/src/jaxws
-# sdkcore
-mkdir -p trading-api-sdkcore
-cp $pwd/trading-api-sdkcore/pom.xml trading-api-sdkcore
-mkdir -p trading-api-sdkcore/src/main/java/com/ebay/sdk
-cp -r source/core/src/com/ebay/sdk/* trading-api-sdkcore/src/main/java/com/ebay/sdk
-mkdir -p trading-api-sdkcore/src/main/java
-cp -r source/helper/src/* trading-api-sdkcore/src/main/java
+module_pids=()
+copy_to_root & module_pids+=($!)
+copy_to_eBLBaseComponents & module_pids+=($!)
+copy_to_sdkcore & module_pids+=($!)
+wait $module_pids
 
-## Fortunately this works because the imports follow a consistent spacing format.  OpenRewrite would have been 
-## preferred to refactor this but attempting use this was successful.
-find trading-api-sdkcore/src/main/java -name *.java | xargs sed -i -e \
-    "s/^import javax\.xml\.ws/import jakarta.xml.ws/" \
-    -e "s/^import javax\.xml\.soap/import jakarta.xml.soap/" \
-    -e "s/^import javax\.xml\.bind/import jakarta.xml.bind/"
-## Drop anything core or helper class that uses a *Call class or a *Type that does not have a source file in the 
-## source repo.  These removed classes may be reintroduced later but will need to be evaluated on a case-by-case basis.
-##
-## Files are removed individually because it is hard to join class references in files with Call/Type classes.
-rm trading-api-sdkcore/src/main/java/com/ebay/sdk/helper/GetCategoryFeaturesHelper.java \
-    trading-api-sdkcore/src/main/java/com/ebay/sdk/helper/cache/CategoriesDownloader.java \
-    trading-api-sdkcore/src/main/java/com/ebay/sdk/helper/cache/DetailsDownloader.java \
-    trading-api-sdkcore/src/main/java/com/ebay/sdk/helper/cache/FeaturesDownloader.java \
-    trading-api-sdkcore/src/main/java/com/ebay/sdk/helper/eBayDetailsHelper1.java \
-    trading-api-sdkcore/src/main/java/com/ebay/sdk/helper/ui/DialogAccount.java
-
-## Temporarily remove anything related to Java AWT + Swing in order to focus first on the API.  API and UI should be 
-## compiled and packaged separately.  Catches import statements but any fully-qualified references will need to be 
-## addressed individually.
-cp $pwd/trading-api-sdkcore/src/main/java/com/ebay/sdk/ApiCall.java $pwd/trading-api-sdkcore/src/main/java/com/ebay/sdk/ApiCredential.java \
-    trading-api-sdkcore/src/main/java/com/ebay/sdk
-find trading-api-sdkcore -name *.java | xargs grep -l -e "^import java\.awt\..*;$" -e  "^import javax\.swing\..*;$" | xargs rm
 # build
 mvn clean install
