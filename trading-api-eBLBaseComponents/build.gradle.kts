cf179a6422df26aeec7092698dadc034b89b2391
Matthew Ouyang <mouyang@users.noreply.github.com>
2025-11-06 13:07:00 -0500

    delegate wsimport to existing Maven build
    existing wsimport plugins for Gradle could not be integrated

---
+++ trading-api-eBLBaseComponents/build.gradle.kts
@@ -0,0 +1,7 @@
+/* This placeholder is needed to inform Gradle that this generates a jar file.  Transitive
+   dependencies will not resolve within Gradle without this.
+ */
+plugins {
+    `java-library`
+    `maven-publish`
+}
