1ac3d0bce0484c49f0fea6c186c6c9dd09161355
Matthew Ouyang <mouyang@users.noreply.github.com>
2025-12-02 18:44:33 -0500

    push up build file for api version 1331, put build under subproject

---
+++ trading-api-version/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+subprojects {
+    apply(from = "${rootDir}/trading-api-core-java/build.gradle.kts")
+    apply(from = "${rootDir}/trading-api-core-maven/build.gradle.kts")
+
+    dependencies {
+        compileOnly(project(":trading-api-sdkcore"))
+        implementation("com.ebay.developer:trading-api-eBLBaseComponents:${project.version}")
+        implementation("org.apache.commons:commons-text:1.9")
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            register<MavenPublication>("apiVersion") {
+                afterEvaluate {
+                    /* By default, the artifactId is the folder name which has the eBay API version in it.  This is not
+                       necessary because it is embeddded in the artifact itself.
+                     */
+                    artifactId = "trading-api-version"
+                    from(components["java"])
+                }
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
+}
