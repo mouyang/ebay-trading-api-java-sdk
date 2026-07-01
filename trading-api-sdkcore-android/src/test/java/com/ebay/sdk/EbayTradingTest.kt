5d2dfd61fb9a3be3eb6e272764372d488d98f797
Matthew Ouyang <mouyang@users.noreply.github.com>
2026-07-01 18:13:06 -0400

    basic unmarshal tests

--- trading-api-sdkcore-android/src/test/java/com/ebay/sdk/EbayTradingTest.kt
+++ trading-api-sdkcore-android/src/test/java/com/ebay/sdk/EbayTradingTest.kt
@@ -1,10 +1,37 @@
 package com.ebay.sdk
 
+import com.ebay.soap.eBLBaseComponents.ItemType
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import java.nio.charset.Charset
 
 class EbayTradingTest {
     @Test
-    fun trivial() {
+    fun unmarshal() {
+        val itemID = "12345"
+        val xml = """
+            <Item>
+                <ItemID>${itemID}</ItemID>
+            </Item>
+        """.trimIndent().toByteArray(Charset.forName("UTF-8"))
+        val item = EbayTrading.unmarshal(xml, ItemType::class.java)
+        assertEquals(itemID, item.itemID)
+    }
+
+    /**
+     * Sometimes eBay provides elements that are no longer valid for a particular API version.
+     * Unmarshalling should not crash because of that.
+     */
+    @Test
+    fun unmarshal_unknownProperties() {
+        val itemID = "12345"
+        val xml = """
+            <Item>
+                <ItemID>${itemID}</ItemID>
+                <UnknownItemProperty>unknownItemProperyValue</UnknownItemProperty>
+            </Item>
+        """.trimIndent().toByteArray(Charset.forName("UTF-8"))
+        val item = EbayTrading.unmarshal(xml, ItemType::class.java)
+        assertEquals(itemID, item.itemID)
     }
 }
